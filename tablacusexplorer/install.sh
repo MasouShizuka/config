ab2092a31bd9e5d8c8f777342d55115fe886f212
MasouShizuka <qwe569746537@qq.com>
2025-09-07 18:18:08 +0800

    fix(tablacusexplorer): install `addons` and `config` to the corresponding paths respectively

--- tablacusexplorer/install.sh
+++ tablacusexplorer/install.sh
@@ -11,5 +11,8 @@ CONFIG_DIR="$DIR/config"
 
 scoop install "$PARENT_DIR/scoop/config/workspace/tablacus-explorer.json"
 
-target="$HOME/scoop/persist/tablacus-explorer"
-install_to_target "$CONFIG_DIR" "$target"
+target="$HOME/scoop/persist/tablacus-explorer/addons"
+install_to_target "$CONFIG_DIR/addons" "$target"
+
+target="$HOME/scoop/persist/tablacus-explorer/config"
+install_to_target "$CONFIG_DIR/config" "$target"
