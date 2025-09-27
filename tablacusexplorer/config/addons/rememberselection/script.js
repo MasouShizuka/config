6f99098e8dcabdffd8ddbddcd250ff01e85bb09d
MasouShizuka <qwe569746537@qq.com>
2025-09-27 20:46:55 +0800

    feat(tablacusexplorer): add rememberselection

---
+++ tablacusexplorer/config/addons/rememberselection/script.js
@@ -0,0 +1,11 @@
+const Addon_Id = "rememberselection";
+if (window.Addon == 1) {
+	$.importScript("addons\\" + Addon_Id + "\\sync.js");
+} else {
+	SetTabContents(0, "", '<label>Number of items</label><br><input type="text" id="Save" class="full" placeholder="1000"><br><input type="button" value="Delete" onclick="Addons.RememberSelection.Delete()">');
+	Addons.RememberSelection = {
+		Delete: async function () {
+			DeleteItem(await OrganizePath(Addon_Id, BuildPath(ui_.DataFolder, "config")) + ".tsv", FOF_ALLOWUNDO);
+		}
+	}
+}
